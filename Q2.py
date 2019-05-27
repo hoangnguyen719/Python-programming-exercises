1dec4977cd9d67cf5624b3958afed6ec84e1a2a0
Hoang Nguyen <hoangnguyen719@users.noreply.github.com>
2019-05-27 00:50:24 -0400

    Change Q2, add Q3

--- Q2.py
+++ Q2.py
@@ -1,1 +1,8 @@
-print(input('asd:'))
+n = int(input('Number: '))
+def factor(x):
+    if x <= 1:
+        return 1
+    else:
+        return x*factor(x-1)
+
+print(factor(n))
---
+++ Q3.py
@@ -0,0 +1,7 @@
+n = int(input('Number: '))
+
+result = {}
+for i in range(1, n+1):
+    result[i] = i**2
+
+print(result)
